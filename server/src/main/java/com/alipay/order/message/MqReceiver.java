103cfd3f8c18e6727ffbac546d63a252221583b0
kangyuhang <1063178819@qq.com>
2019-06-19 17:38:45 +0800

    测试 conflix

--- server/src/main/java/com/alipay/order/message/MqReceiver.java
+++ server/src/main/java/com/alipay/order/message/MqReceiver.java
@@ -33,7 +33,7 @@ public class MqReceiver {
             value = @Queue("computer")
     ))
     public void processComputer(String message) {
-        System.out.println("这是康宇航的消息" + message);
+        System.out.println("这是kangyuhang的消息" + message);
     }
 
 
